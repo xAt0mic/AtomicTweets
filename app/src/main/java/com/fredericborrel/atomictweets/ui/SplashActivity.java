3465954e29bfafb73aee04a077f76e95349f74b7
Frederic Borrel <frederic.borrel@gmail.com>
2017-06-14 11:41:31 +0200

    Initial commit with project base

---
+++ app/src/main/java/com/fredericborrel/atomictweets/ui/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.fredericborrel.atomictweets.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Frederic on 14/06/2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, AuthActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
